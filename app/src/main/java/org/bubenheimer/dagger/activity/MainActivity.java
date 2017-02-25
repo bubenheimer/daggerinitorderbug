7b5c7d6b8152afe98bf0a85ca740347f40b1a960
Uli <ububen@gmail.com>
2017-02-24 23:17:04 -0600

    Remove my workaround to actually let it crash

--- app/src/main/java/org/bubenheimer/dagger/activity/MainActivity.java
+++ app/src/main/java/org/bubenheimer/dagger/activity/MainActivity.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import org.bubenheimer.dagger.R;
 import org.bubenheimer.dagger.fragment.MainFragment;
 
-import dagger.android.AndroidInjection;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public final class MainActivity extends DaggerAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
